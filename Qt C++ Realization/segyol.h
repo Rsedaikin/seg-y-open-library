45248b00256e7ea355823758e2df814c04ac235e
rsedaikin <rsedaikin@gmail.com>
2017-10-04 10:18:25 +0500

    Added copyright and license file

--- Qt C++ Realization/segyol.h
+++ Qt C++ Realization/segyol.h
@@ -1,3 +1,23 @@
+/*
+Copyright 2017, Sedaikin Roman Dmitrievich
+
+segyol.h is part of SEG-Y Open Library.
+
+SEG-Y Open Library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SEG-Y Open Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SEG-Y Open Library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef SEGYOL_H
 #define SEGYOL_H
 #include <QFile>
